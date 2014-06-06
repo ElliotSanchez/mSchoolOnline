1920826f5622e925d0d380689feb3825e37dfebe
Barrett Conrad <bconrad@cotingasoft.com>
2014-06-06 15:16:50 -0500

    added 'Done' button and modal to student' pathway menu;

---
+++ public/assets/mschool/js/pathway.js
@@ -0,0 +1,30 @@
+$(function(){
+
+    $('#pathway-done').click(function(e) {
+
+        bootbox.dialog({
+            message: "Click the <span class=\"btn btn-xs\">I\'m done</span> button if you are completely finished with your work.",
+            title: "Are you sure you are done?",
+            buttons: {
+
+                ok: {
+                    label: "I'm done",
+                    className: "btn-default",
+                    callback: function () { window.location.replace("/pathway/finished") }
+                },
+                main: {
+                    label: "I'm not done",
+                    className: "btn-success",
+                    callback: null
+                },
+
+            }
+        });
+
+        e.stopPropagation();
+
+        return false;
+
+    })
+
+});
