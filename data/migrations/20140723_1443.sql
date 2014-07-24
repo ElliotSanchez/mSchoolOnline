bcfaaa05b8477baece4d96e57cd2120216dc696c
Barrett Conrad <bconrad@cotingasoft.com>
2014-07-24 11:26:59 -0500

    updated to learning_points view SQL definitions;

--- data/migrations/20140723_1443.sql
+++ data/migrations/20140723_1443.sql
@@ -1,17 +1,17 @@
 CREATE VIEW dreambox_usage_lp AS
-SELECT student_id, MAX(unique_lessons_completed) AS learning_points
+SELECT student_id, MAX(unique_lessons_completed * 5) AS learning_points
 FROM dreambox_usage
 GROUP BY student_id
-ORDER BY student_id ASC;
+ORDER BY student_id;
 
 CREATE VIEW stmath_progress_lp AS
-SELECT student_id, MAX(CAST(syllabus_progress AS UNSIGNED INT)) * 8 AS learning_points
+SELECT student_id, MAX(CAST(syllabus_progress AS UNSIGNED INT) * 8) AS learning_points
 FROM stmath_progress
 GROUP BY student_id
 ORDER BY student_id ASC;
 
 CREATE VIEW ttm_overview_lp AS
-SELECT student_id, MAX(CAST(total_lessons_passed AS UNSIGNED INT)) * 15 AS learning_points
+SELECT student_id, MAX(CAST(total_lessons_passed AS UNSIGNED INT) * 15) AS learning_points
 FROM ttm_overview
 GROUP BY student_id
 ORDER BY student_id ASC;
