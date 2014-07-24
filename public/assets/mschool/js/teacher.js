e9b0d152756538351a6a7e0fe2c16f0d403db1a5
Barrett Conrad <bconrad@cotingasoft.com>
2014-07-23 22:37:25 -0500

    first steps toward an iready graph;
    committing some garbage code and comments before immediately deleting them;

--- public/assets/mschool/js/teacher.js
+++ public/assets/mschool/js/teacher.js
@@ -8,6 +8,110 @@ $(function() {
         window.location = 'http://' + host + url + '/' + id;
     });
 
+    $('div.assessment-class-average').each(function() {
+
+        var mclassId = $(this).data('mclass-id');
+
+//        chartWidth = 600;
+//        chartHeight = 500; // MODIFIED BELOW BASED ON DATA
+//        numRecords = 24;
+
+//        (function(){
+
+            //var canvasSelector = 'div.assessment-class-average'; // TODO MERGE WITH ABOVE USAGE
+
+        var rangeMap = {
+            'K' : { 'lower': {'min':201 ,	'max':403}, 'upper': {'min': 451, 	'max': 499}},
+            '1' : { 'lower': {'min':406 ,	'max':425}, 'upper': {'min': 474, 	'max': 523}},
+            '2' : { 'lower': {'min':428 ,	'max':447}, 'upper': {'min': 494.5,	'max': 542}},
+            '3' : { 'lower': {'min':450 ,	'max':469}, 'upper': {'min': 516, 	'max': 563}},
+            '4' : { 'lower': {'min':471.5,	'max':490}, 'upper': {'min': 532, 	'max': 574}},
+            '5' : { 'lower': {'min':487.5,	'max':501}, 'upper': {'min': 542.5, 'max': 584}},
+            '6' : { 'lower': {'min':497 ,	'max':509}, 'upper': {'min': 518, 	'max': 527}},
+            '7' : { 'lower': {'min':518 ,	'max':528}, 'upper': {'min': 532, 	'max': 536}},
+            '8' : { 'lower': {'min':532 ,	'max':537}, 'upper': {'min': 546, 	'max': 555}},
+            '9' : { 'lower': {'min':546 ,	'max':556}, 'upper': {'min': 591, 	'max': 626}},
+            '10': { 'lower': {'min':571	,	'max':587}, 'upper': {'min': 612,	'max': 637}},
+            '11': { 'lower': {'min':586.5 ,	'max':602}, 'upper': {'min': 627, 	'max': 652}},
+            '12': { 'lower': {'min':601.5 ,	'max':617}, 'upper': {'min': 708.5,	'max': 800}}
+            };
+
+        var rangeBounds = rangeMap['7'];
+
+        var rangeMin = Math.floor(rangeBounds.lower.min/100)*100;
+        var rangeMax = Math.ceil(rangeBounds.upper.max/100)*100;
+
+        console.log(rangeBounds)
+        console.log(rangeMin);
+        console.log(rangeMax);
+
+        var square = 400;
+
+        //var scale = square / rangeMax;
+        //console.log(scale);
+
+        console.log(square);
+            var svg = d3.select("div.assessment-class-average").append("svg")
+                    .attr("width", square)
+                    .attr("height", square);
+
+        var y0 = d3.scale.linear();
+
+        y0.domain([rangeBounds.lower.min, rangeBounds.upper.max]);
+        y0.range([square, 0]); // INVERTED
+
+//            y1 = d3.scale.linear().domain([20, 80]).range([height, 0]);
+
+//        var xAxis = d3.svg.axis()
+//            .scale(x)
+//            .orient("bottom");
+
+        // create left yAxis
+        //var yAxisLeft = d3.svg.axis().scale(y0).ticks(4).orient("left");
+        // create right yAxis
+        //var yAxisRight = d3.svg.axis().scale(y1).ticks(6).orient("right");
+
+        var yAxis = d3.svg.axis().scale(y0).orient("left");
+
+        console.log('y1 '+ rangeBounds.upper.max);
+        console.log('y1 '+ y0(rangeBounds.upper.max));
+
+            var line1 = svg.append("line")
+                .attr("x1", 0)
+                .attr("y1", y0(rangeBounds.lower.max))
+                .attr("x2", square)
+                .attr("y2", y0(rangeBounds.upper.max))
+                .attr("stroke-width", 1)
+                .attr("stroke", "gray");
+
+            var line2 = svg.append("line")
+                .attr("x1", 0)
+                .attr("y1", y0(rangeBounds.lower.min))
+                .attr("x2", square)
+                .attr("y2", y0(rangeBounds.upper.min))
+                .attr("stroke-width", 1)
+                .attr("stroke", "gray");
+
+//            var line1 = svg.append("line")
+//                .attr("x1", 0)
+//                .attr("y1", Math.floor(square-rangeBounds.lower.max*scale))
+//                .attr("x2", square)
+//                .attr("y2", Math.floor(square-rangeBounds.upper.max*scale))
+//                .attr("stroke-width", 1)
+//                .attr("stroke", "gray");
+//
+//            var line2 = svg.append("line")
+//                .attr("x1", 0)
+//                .attr("y1", Math.floor(square-rangeBounds.lower.min*scale))
+//                .attr("x2", square)
+//                .attr("y2", Math.floor(square-rangeBounds.upper.min*scale))
+//                .attr("stroke-width", 1)
+//                .attr("stroke", "gray");
+
+//        })();
+
+    });
+
     // STUDENT PLACEMENTS GRAPH
     $('div.assessment-student-placement').each(function() {
 
